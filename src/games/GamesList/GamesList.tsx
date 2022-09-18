cacaca73edb41d6035c009115608174ea042e94f
Alex <pudo-aleksej@yandex.ru>
2022-09-18 10:47:49 +0300

    add routers

--- src/games/GamesList/GamesList.tsx
+++ src/games/GamesList/GamesList.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import ErrorBoundary from '../../pages/ErrorBoundarry';
  function GamesList() {
     let params = useParams();
-    const Component = React.lazy(() => import(`./${params.gameId}/Game.tsx`));
+    const Component = React.lazy(() => import(`../${params.gameId}/Game.tsx`));
     return (
         <div>
       <Suspense fallback={<div>Loading...</div>}>
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,25 +3,43 @@ import Games from "../pages/Games/Games";
 import MainPage from "../pages/Main/MainPage";
 import GamesList from "../games/GamesList/GamesList";
 const routes = {
-  node: { virtual: true },
+  node: { virtual: true, path: "" },
   children: [
     {
-        node: {path: "/", component: <MainPage/>}
+      node: { path: "/", component: <MainPage /> },
     },
     {
       node: { path: "/games", component: <Games /> },
-      children: [{ node: "/:game" }],
+      children: [{ node: {path: "/:gameId", component: <GamesList />} }],
     },
   ],
 };
+let treeRoutes = [];
+(function makeTreeRoutes(routes, parrentRoutePath) {
+  if (routes.children) {
+     routes.children.forEach((route) => makeTreeRoutes(route, routes.node.path));
+  } 
+  if (!routes?.node?.virtual) {
+    treeRoutes.push(<Route
+      key={`${parrentRoutePath}${routes.node.path}`}
+      path={`${parrentRoutePath}${routes.node.path}`}
+      element={routes.node.component}
+    />)
+  }
+  
+})(routes, "")
+
 function RouterComponent() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/:gameId" element={<GamesList />}></Route>
-      <Route path="/games" element={<Games />}></Route>
-    </Routes>
-  );
+return (
+  <Routes>
+{treeRoutes}
+</Routes>
+)
+//   <Routes>
+//   <Route path="/" element={<MainPage />} />
+//   <Route path="/:gameId" element={<GamesList />}></Route>
+//   <Route path="/games" element={<Games />}></Route>
+// </Routes>
 }
 
 export default RouterComponent;
